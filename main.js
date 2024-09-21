74d6cab86e72f13a12bbb4c05bb758570925427b
Mauricio <maurifonsecacode@gmail.com>
2024-09-21 15:49:55 -0300

    Mauri - actualizacion JS y estilos presupuesto

--- main.js
+++ main.js
@@ -1,67 +1,142 @@
-let usuario = prompt("Ingresar nombre");
-alert("Bienvenid@ " + usuario);
-function introduccion(){
-    let continuar = confirm("A continuación se le van a pedir datos personales sobre ingresos y gastos mensuales, desea continuar?")
-    if (!continuar) {
-        alert (usuario + ", necesitamos su confirmacion para continuar")
-        return false
-    }else {
-        alert ("Perfecto continuemos " + usuario)
-        return true
-    }    
-}
-if (!introduccion()) {
-    throw new Error("El usuario ha detenido el proceso")
-}
-function obtenerIngreso(mensaje) {
-    let ingreso
-    do {
-        ingreso = parseFloat(prompt(mensaje))
-        if (isNaN(ingreso) || ingreso < 0) {
-            alert("Ingresar datos en valores")
-        }
-    } while (isNaN(ingreso) || ingreso < 0)
-    return ingreso
-}
-let fuenteIngresoUno = obtenerIngreso("Ingresar fuente de ingresos principal (en valores):")
-console.log("Ingreso principal:", fuenteIngresoUno)
-let fuenteIngresoDos = obtenerIngreso("Ingresar fuente de ingresos secundaria (en valores):")
-console.log("Ingreso secundario:", fuenteIngresoDos)
-const sumar = (ingreso1, ingreso2) => ingreso1 + ingreso2
-let totalIngresos = sumar(fuenteIngresoUno, fuenteIngresoDos)
-console.log("Total de ingresos:", totalIngresos)
-alert("El total de tus ingresos es: " + totalIngresos)
-function obtenerGasto(mensaje) {
-    let gasto
-    do {
-        gasto = parseFloat(prompt(mensaje))
-        if (isNaN(gasto) || gasto < 0) {
-            alert("Ingresar datos en valores")
-        }
-    } while (isNaN(gasto) || gasto < 0)
-    return gasto
-}
+//INGRESOS
+const ingresos = ["Fuente ingreso 1", "Fuente ingreso 2", "Suma ingresos"]
+const listaDesordenada = document.querySelector("ul")
+const elementoIngresos = []
+const inputsFuente1 = []
+const inputsFuente2 = []
+const inputsSuma = []
+ingresos.forEach ((ingreso, index) => {
+    const ingresoPresupuesto = document.createElement ("li")
+    ingresoPresupuesto.innerText = ingreso
+    const contenedorInputs = document.createElement("div")
+    for (let i = 0; i < 12; i++) {
+        const inputTexto = document.createElement("input")
+        inputTexto.type = "number"
+        if (index === 0) {
+            inputsFuente1.push(inputTexto);
+          } else if (index === 1) {
+            inputsFuente2.push(inputTexto);
+          } else if (index === 2) {
+            inputTexto.readOnly = true
+            inputsSuma.push(inputTexto)
+          }
+    contenedorInputs.appendChild(inputTexto)
+    }
+    ingresoPresupuesto.appendChild(contenedorInputs)
+    elementoIngresos.push(ingresoPresupuesto) 
+})
+elementoIngresos.forEach((el) => {
+    listaDesordenada.appendChild(el)
+})
+function calcularSuma() {
+    for (let i = 0; i < 12; i++) {
+      const valorFuente1 = parseFloat(inputsFuente1[i].value) || 0
+      const valorFuente2 = parseFloat(inputsFuente2[i].value) || 0
+      const suma = valorFuente1 + valorFuente2
+      inputsSuma[i].value = suma
+    }
+  }
+  inputsFuente1.forEach((input) => {
+    input.addEventListener("input", calcularSuma)
+  })
+  inputsFuente2.forEach((input) => {
+    input.addEventListener("input", calcularSuma)
+  })
+
+//GASTOS
+  const gastos = ["Ahorro mensual", "Alquiler", "Transporte", "Tarjeta de crédito principal", "Tarjeta de crédito secundaria", "Seguros contratados", "Luz y agua", "Telefonía", "Internet", "Educación", "Suma Gastos"]
+  const listaDesordenadaDos = document.querySelector("ul")
+  const elementoGastos = []
+  const inputsGastos = []
+  const inputsSumaDos = []
+  gastos.forEach ((gasto, index) => {
+      const gastoPresupuesto = document.createElement ("li")
+      gastoPresupuesto.innerText = gasto
+      const contenedorInputsDos = document.createElement("div")
+      for (let i = 0; i < 12; i++) {
+          const inputTexto = document.createElement("input")
+          inputTexto.type = "number"
+          if (index === gastos.length - 1) {
+            inputTexto.readOnly = true
+            inputsSumaDos.push(inputTexto)
+            } else {
+            if (!inputsGastos[index]) {
+                inputsGastos[index] = []
+                }
+            inputsGastos[index].push(inputTexto) 
+            }   
+        contenedorInputsDos.appendChild(inputTexto)
+      }
+      gastoPresupuesto.appendChild(contenedorInputsDos);
+      elementoGastos.push(gastoPresupuesto) 
+  })
+  elementoGastos.forEach((el) => {
+    listaDesordenadaDos.appendChild(el)
+  })
+  function calcularSumaDos() {
+    for (let i = 0; i < 12; i++) {
+      let sumaDos = 0
+      inputsGastos.forEach((inputArray) => {
+        const valorGasto = parseFloat(inputArray[i].value) || 0
+        sumaDos += valorGasto
+      });
+      inputsSumaDos[i].value = sumaDos
+    }
+  }
+inputsGastos.forEach((inputArray) => {
+    inputArray.forEach((input) => {
+      input.addEventListener("input", calcularSumaDos)
+    })
+  })
+const inputTextField = document.getElementById ("identificacionUsuario")
+const boton = document.getElementById('inicio');
+inputTextField.addEventListener("change", function(){
+    boton.style.backgroundColor = "#13e813";
+})
+listaDesordenadaTres.append(...elementoMeses)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* const listaMeses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Setiembre", "Octubre", "Noviembre", "Diciembre"]
+const listaDesordenadaTres = document.querySelector("tr")
+const elementoMeses = []
+listaMeses.forEach (listaMeses => {
+    const mesesPresupuesto = document.createElement ("th")
+    mesesPresupuesto.innerText = listaMeses, elementoMeses.push (mesesPresupuesto)
+}) */
+//EL nombre se deberia guardar en local storage
+
+
+{/* 
+
+
 const calcularAhorroMensual = (totalIngresos) => totalIngresos * 0.1
 let ahorroMensual = calcularAhorroMensual(totalIngresos)
 console.log("Ahorros mensuales:", ahorroMensual)
-alert("Usted deberia fijar su meta de ahorro mensual en: " + ahorroMensual)
-let detallesGastos = [
-    obtenerGasto("Cuánto gastas en alquiler (en valores):"),
-    obtenerGasto("Cuánto gastas en transporte (en valores):"),
-    obtenerGasto("Monto de tarjeta de crédito principal (en valores):"),
-    obtenerGasto("Monto de tarjeta de crédito secundaria (en valores):"),
-    obtenerGasto("Cuánto gastas en seguros contratados (en valores):"),
-    obtenerGasto("Cuánto gastas en luz y agua (en valores):"),
-    obtenerGasto("Cuánto gastas en telefonía (en valores):"),
-    obtenerGasto("Cuánto gastas en internet (en valores):"),
-    obtenerGasto("Cuánto invertís en educación al mes (en valores):"),
-]
-detallesGastos.push(ahorroMensual)
-const sumarGastos = (array) => array.reduce((acc, val) => acc + val, 0)
-let totalGastos = sumarGastos (detallesGastos)
-console.log("Total de gastos:", totalGastos)
-alert("El total de tus gastos es: " + totalGastos)
+alert("Usted deberia fijar su meta de ahorro mensual en: " + ahorroMensual) 
+
+
 const resultadoFinal = (ingresoTotal, gastoTotal) => ingresoTotal - gastoTotal
 let libreDisponibilidad = resultadoFinal(totalIngresos, totalGastos)
 console.log("Libre Disponibilidad:", libreDisponibilidad)
 alert("Tu monto de libre disponibilidad es: " + libreDisponibilidad)
+ */}
